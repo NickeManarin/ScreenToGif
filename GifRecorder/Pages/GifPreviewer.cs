1ee5b2542b7644c87ea22574f1841c4784659d81
freaksterrao <freaksterrao@yahoo.co.in>
2014-07-19 20:13:50 +0530

    Added missing file GIFPreviewer

---
+++ GifRecorder/Pages/GifPreviewer.cs
@@ -0,0 +1,39 @@
+﻿#region
+
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+#endregion
+
+namespace ScreenToGif.Pages
+{
+    public partial class GifPreviewer : Form
+    {
+        public GifPreviewer()
+        {
+            InitializeComponent();
+        }
+
+        public GifPreviewer(string imagePath):this()
+        {
+            if (File.Exists(imagePath))
+            {
+                Bitmap image = new Bitmap(imagePath);
+                pictureBox.Image = image;
+            }
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            Image image = pictureBox.Image;
+            if (null != image)
+            {
+                image.Dispose();
+            }
+
+            base.OnClosing(e);
+        }
+    }
+}
