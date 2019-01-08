886f1e121fd0b6f6c73f43aabb18299fbe2568de
Nicke Manarin <nicke@outlook.com.br>
2019-01-08 20:26:52 -0200

    Oooops.

--- ScreenToGif/Properties/AssemblyInfo.cs
+++ ScreenToGif/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Nicke Manarin")]
 [assembly: AssemblyProduct("ScreenToGif")]
-[assembly: AssemblyCopyright("Copyright© Nicke Manarin 2018")]
+[assembly: AssemblyCopyright("Copyright© Nicke Manarin 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -50,5 +50,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.15.1.0")]
-[assembly: AssemblyFileVersion("2.15.1.0")]
+[assembly: AssemblyVersion("2.15.2.0")]
+[assembly: AssemblyFileVersion("2.15.2.0")]
