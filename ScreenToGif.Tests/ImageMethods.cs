40bf071c38ca82a878345b322c864a24578192c5
Anders Hörnfeldt <anders.hornfeldt@eurocon.se>
2021-04-23 10:04:42 +0200

    Add test for CalculateDifference

---
+++ ScreenToGif.Tests/ImageMethods.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+using ScreenToGif.Model;
+
+using Xunit;
+
+namespace ScreenToGif.Tests
+{
+    public class ImageMethods
+    {
+        [Fact]
+        public void CanCalculateDifference()
+        {
+            var f1 = new FrameInfo()
+            {
+                Path = "./TestData/b1.bmp",
+                Index = 0
+            };
+
+            var f2 = new FrameInfo()
+            {
+                Path = "./TestData/b2.bmp",
+                Index = 1
+            };
+
+            var diff = ImageUtil.ImageMethods.CalculateDifference(f1, f2);
+
+            Assert.Equal(25, diff);
+        }
+    }
+}
