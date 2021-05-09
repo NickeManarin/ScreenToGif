86cecc701131a8419c1666434b083d2f6ed3e6f7
Mateusz Bogusz <mabakay@o2.pl>
2021-05-09 13:29:23 +0200

    Hiding last panel after storyboard completes

--- ScreenToGif/Windows/Editor.xaml.cs
+++ ScreenToGif/Windows/Editor.xaml.cs
@@ -227,7 +227,10 @@ namespace ScreenToGif.Windows
         public Editor()
         {
             InitializeComponent();
-            
+
+            // Hide last displayed panel after the panel closing animation is complete.
+            this.FindStoryboard("HideOverlayGridStoryboard").Completed += HideLastDisplayedPanel;
+
             #region Adjust the position
 
             //Tries to adjust the position/size of the window, centers on screen otherwise.
@@ -4732,6 +4735,11 @@ namespace ScreenToGif.Windows
             ShapeDrawingCanvas.DeselectAll();
         }
 
+        private void HideLastDisplayedPanel(object sender, EventArgs e)
+        {
+            HideAllVisibleGrids();
+        }
+
         private void ClosePanel(bool isCancel = false, bool removeEvent = false)
         {
             StatusList.Remove(StatusType.Warning);
@@ -4749,8 +4757,6 @@ namespace ScreenToGif.Windows
 
             BeginStoryboard(this.FindStoryboard("HidePanelStoryboard"), HandoffBehavior.Compose);
             BeginStoryboard(this.FindStoryboard("HideOverlayGridStoryboard"), HandoffBehavior.Compose);
-
-            HideAllVisibleGrids();
         }
 
         private List<int> SelectedFramesIndex()
