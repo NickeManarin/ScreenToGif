4e4317526d29280906e39360c9f55c38f2a81003
Nicke Manarin <nicke@outlook.com.br>
2021-05-14 23:26:13 -0300

    891 Version bump.

--- ScreenToGif/Properties/AssemblyInfo.cs
+++ ScreenToGif/Properties/AssemblyInfo.cs
@@ -50,5 +50,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.30.0.0")]
-[assembly: AssemblyFileVersion("2.30.0.0")]
+[assembly: AssemblyVersion("2.30.1.0")]
+[assembly: AssemblyFileVersion("2.30.1.0")]
