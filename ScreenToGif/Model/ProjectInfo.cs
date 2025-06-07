10ac00df99345e1243bd05b73d8ff3f355084496
Nicke Manarin <nicke@outlook.com.br>
2025-06-07 16:05:44 -0300

    Fixed issue with partial export ignoring expression ordering of the indexes.

--- ScreenToGif/Model/ProjectInfo.cs
+++ ScreenToGif/Model/ProjectInfo.cs
@@ -7,7 +7,6 @@ using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Windows;
 using ScreenToGif.Domain.Enums;
-using ScreenToGif.Domain.Interfaces;
 using ScreenToGif.Domain.Models;
 using ScreenToGif.Util;
 using ScreenToGif.Util.Codification;
@@ -26,7 +25,7 @@ public class ProjectInfo
     public string RelativePath { get; set; }
 
     /// <summary>
-    /// The date of reation of this project.
+    /// The date of creation of this project.
     /// </summary>
     [DataMember(Order = 1)]
     public DateTime CreationDate { get; set; } = DateTime.Now;
@@ -127,8 +126,7 @@ public class ProjectInfo
 
         Directory.CreateDirectory(FullPath);
 
-        #region Create ActionStack folders
-
+        //Create ActionStack folders
         if (!Directory.Exists(ActionStackPath))
             Directory.CreateDirectory(ActionStackPath);
 
@@ -137,8 +135,6 @@ public class ProjectInfo
 
         if (!Directory.Exists(RedoStackPath))
             Directory.CreateDirectory(RedoStackPath);
-
-        #endregion
 
         CreateMutex();
 
@@ -149,14 +145,12 @@ public class ProjectInfo
     {
         try
         {
-            using (var ms = new MemoryStream())
-            {
-                var ser = new DataContractJsonSerializer(typeof(ProjectInfo));
+            using var ms = new MemoryStream();
 
-                ser.WriteObject(ms, this);
+            var ser = new DataContractJsonSerializer(typeof(ProjectInfo));
+            ser.WriteObject(ms, this);
 
-                File.WriteAllText(path ?? ProjectPath, Encoding.UTF8.GetString(ms.ToArray())); //Use Serializer
-            }
+            File.WriteAllText(path ?? ProjectPath, Encoding.UTF8.GetString(ms.ToArray())); //Use Serializer
         }
         catch (Exception ex)
         {
@@ -192,7 +186,7 @@ public class ProjectInfo
     public void CreateMutex()
     {
         //TODO: Validate the possibility of opening this project.
-        //I need to make sure that i'll release the mutexes.
+        //I need to make sure that I'll release the mutexes.
 
         MutexList.Add(RelativePath);
     }
@@ -202,176 +196,54 @@ public class ProjectInfo
         MutexList.Remove(RelativePath);
     }
 
-    /// <summary>
-    /// Copy all necessary files to a new encode folder.
-    /// </summary>
-    /// <param name="copyJson">True if the Project.json file should be copied too.</param>
-    /// <param name="useBytes">True if the images should be converted to byte array.</param>
-    /// <param name="usePadding">True if the file names should have a left pad, to preserve the file ordering.</param>
-    /// <returns>A list of frames with the new path.</returns>
-    internal ExportProject CopyToExport(bool copyJson = false, bool useBytes = false, bool usePadding = false)
-    {
-        Persist();
-
-        var export = new ExportProject();
-
-        #region Output folder
-
-        export.Path = Path.Combine(FullPath, "Encode " + DateTime.Now.ToString("yyyy-MM-dd hh-mm-ss-ff"));
-
-        if (!Directory.Exists(export.Path))
-            Directory.CreateDirectory(export.Path);
-
-        #endregion
-
-
-        if (useBytes)
-        {
-            export.Frames = new List<ExportFrame>();
-            export.ChunkPath = Path.Combine(export.Path, "Chunk");
-            export.NewChunkPath = Path.Combine(export.Path, "NewChunk");
-
-            try
-            {
-                //Create chunk file.
-                using (var fileStream = new FileStream(export.ChunkPath, FileMode.Create, FileAccess.Write, FileShare.None))
-                {
-                    var pos = 0L;
-
-                    foreach (var info in Frames)
-                    {
-                        var image = new PixelUtil(info.Path.SourceFrom());
-                        image.LockBits();
-
-                        fileStream.WriteBytes(image.Pixels);
-
-                        export.Frames.Add(new ExportFrame
-                        {
-                            DataPosition = pos,
-                            DataLength = image.Pixels.LongLength,
-                            Delay = info.Delay,
-                            Rect = new Int32Rect(0,0, image.Width, image.Height),
-                            ImageDepth = image.Depth
-                        });
-
-                        //Advances in the position.
-                        pos += image.Pixels.LongLength;
-
-                        image.UnlockBitsWithoutCommit();
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                LogWriter.Log(e, "It was impossible to get the image bytes to encode.");
-                throw;
-            }
-
-            return export;
-        }
-
-        export.UsesFiles = true;
-        export.FramesFiles = new List<IFrame>();
-
-        try
-        {
-            #region If it's being exported as project, maintain file naming
-
-            if (copyJson)
-            {
-                foreach (var info in Frames)
-                {
-                    var filename = Path.Combine(export.Path, Path.GetFileName(info.Path));
-
-                    //Copy the image to the folder.
-                    File.Copy(info.Path, filename, true);
-
-                    //Create the new object and add to the list.
-                    export.FramesFiles.Add(new FrameInfo(filename, info.Delay));
-                }
-
-                File.Copy(ProjectPath, Path.Combine(export.Path, "Project.json"), true); //TODO: Filter out json too.
-
-                return export;
-            }
-
-            #endregion
-
-            //Detect pad size.
-            var pad = usePadding ? (Frames.Count - 1).ToString().Length : 0;
-
-            foreach (var info in Frames)
-            {
-                //Changes the path of the image. Writes as an ordered list of files, replacing the old filenames.
-                var filename = Path.Combine(export.Path, export.FramesFiles.Count.ToString().PadLeft(pad, '0') + ".png");
-
-                //Copy the image to the folder.
-                File.Copy(info.Path, filename, true);
-
-                //Create the new object and add to the list.
-                export.FramesFiles.Add(new FrameInfo(filename, info.Delay));
-            }
-        }
-        catch (Exception ex)
-        {
-            LogWriter.Log(ex, "It was impossible to copy the files to encode.");
-            throw;
-        }
-
-        return export;
-    }
-
     internal ExportProject CopyToExport(List<int> indexes, bool copyJson = false, bool useBytes = false, bool usePadding = false)
     {
         Persist();
 
         var export = new ExportProject();
 
-        #region Output folder
-
+        //Output folder
         var encodePath = "Encode " + DateTime.Now.ToString("yyyy-MM-dd hh-mm-ss-ff");
         export.Path = Path.Combine(FullPath, encodePath);
 
         if (!Directory.Exists(export.Path))
             Directory.CreateDirectory(export.Path);
 
-        #endregion
+        var items = indexes.Count > 0 ? indexes.Select(i => Frames[i]).ToList() : Frames;
 
         if (useBytes)
         {
-            export.Frames = new List<ExportFrame>();
+            export.Frames = [];
             export.ChunkPath = Path.Combine(export.Path, "Chunk");
             export.NewChunkPath = Path.Combine(export.Path, "NewChunk");
 
             try
             {
                 //Create chunk file.
-                using (var fileStream = new FileStream(export.ChunkPath, FileMode.Create, FileAccess.Write, FileShare.None))
+                using var fileStream = new FileStream(export.ChunkPath, FileMode.Create, FileAccess.Write, FileShare.None);
+                var pos = 0L;
+
+                foreach (var info in items)
                 {
-                    var pos = 0L;
+                    var image = new PixelUtil(info.Path.SourceFrom());
+                    image.LockBits();
 
-                    foreach (var info in (indexes.Count > 0 ? Frames.Where((w, i) => indexes.Contains(i)) : Frames))
+                    fileStream.WriteBytes(image.Pixels);
+
+                    export.Frames.Add(new ExportFrame
                     {
-                        var image = new PixelUtil(info.Path.SourceFrom());
-                        image.LockBits();
+                        Index = info.Index,
+                        DataPosition = pos,
+                        DataLength = image.Pixels.LongLength,
+                        Delay = info.Delay,
+                        Rect = new Int32Rect(0, 0, image.Width, image.Height),
+                        ImageDepth = image.Depth
+                    });
 
-                        fileStream.WriteBytes(image.Pixels);
+                    //Advances in the position.
+                    pos += image.Pixels.LongLength;
 
-                        export.Frames.Add(new ExportFrame
-                        {
-                            Index = info.Index,
-                            DataPosition = pos,
-                            DataLength = image.Pixels.LongLength,
-                            Delay = info.Delay,
-                            Rect = new Int32Rect(0, 0, image.Width, image.Height),
-                            ImageDepth = image.Depth
-                        });
-
-                        //Advances in the position.
-                        pos += image.Pixels.LongLength;
-
-                        image.UnlockBitsWithoutCommit();
-                    }
+                    image.UnlockBitsWithoutCommit();
                 }
             }
             catch (Exception e)
@@ -384,17 +256,17 @@ public class ProjectInfo
         }
 
         export.UsesFiles = true;
-        export.FramesFiles = new List<IFrame>();
-
+        export.FramesFiles = [];
+        
         try
         {
             #region If it's being exported as project, maintain file naming
 
             if (copyJson)
             {
-                foreach (var info in (indexes.Count > 0 ? Frames.Where((w, i) => indexes.Contains(i)) : Frames))
+                foreach (var info in items)
                 {
-                    var filename = Path.Combine(export.Path, Path.GetFileName(info.Path));
+                    var filename = Path.Combine(export.Path, export.FramesFiles.Count + ".png"); //Path.GetFileName(info.Path)
 
                     //Copy the image to the folder.
                     File.Copy(info.Path, filename, true);
@@ -402,7 +274,7 @@ public class ProjectInfo
                     //Create the new object and add to the list.
                     export.FramesFiles.Add(new FrameInfo
                     {
-                        Index = info.Index,
+                        Index = export.FramesFiles.Count,
                         Path = filename,
                         Delay = info.Delay
                     });
@@ -413,7 +285,7 @@ public class ProjectInfo
                 {
                     var projectAux = ShallowCopy();
                     projectAux.RelativePath = encodePath;
-                    projectAux.Frames = Frames.Where((w, i) => indexes.Contains(i)).ToList();
+                    projectAux.Frames = export.FramesFiles.OfType<FrameInfo>().ToList();
                     projectAux.Persist(Path.Combine(export.Path, "Project.json"));
                 }
                 else
@@ -427,9 +299,9 @@ public class ProjectInfo
             #endregion
 
             //Detect pad size.
-            var pad = usePadding ? (Frames.Count - 1).ToString().Length : 0;
+            var pad = usePadding ? (items.Count - 1).ToString().Length : 0;
 
-            foreach (var info in (indexes.Count > 0 ? Frames.Where((w, i) => indexes.Contains(i)) : Frames))
+            foreach (var info in items)
             {
                 //Changes the path of the image. Writes as an ordered list of files, replacing the old filenames.
                 var filename = Path.Combine(export.Path, export.FramesFiles.Count.ToString().PadLeft(pad, '0') + ".png");
@@ -440,7 +312,7 @@ public class ProjectInfo
                 //Create the new object and add to the list.
                 export.FramesFiles.Add(new FrameInfo
                 {
-                    Index = info.Index,
+                    Index = export.FramesFiles.Count,
                     Path = filename,
                     Delay = info.Delay
                 });
