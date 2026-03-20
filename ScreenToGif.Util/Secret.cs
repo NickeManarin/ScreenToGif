a89e1165f6cc2fea0a63512c87af1eec82e1d0f6
Nicke Manarin <nicke@outlook.com.br>
2026-03-19 21:47:36 -0300

    Fixing the rest of the missing fields

--- ScreenToGif.Util/Secret.cs
+++ ScreenToGif.Util/Secret.cs
@@ -2,20 +2,20 @@ namespace ScreenToGif.Util;
 
 public static class Secret
 {
-    public static string ServerAddress => "";
+    public static string ServerAddress => "https://screentogif-relay-app.azurewebsites.net"; //"https://localhost:5001";
 
-    public static string Email => "";
+    public static string Email => "screentogif@outlook.com";
 
-    public static string Password => "";
+    public static string Password => "AZRew2p?z:9PLpDg|R2wxjZMt4m5283Fg|FpzAQaK3QSFSBdaY|xGP@W#C7qHuqA^MT|pzBC4F^n*NHNQm6_";
 
-    public static int Port => 0;
+    public static int Port => 587; //Or 25
 
-    public static string Host => "";
+    public static string Host => "smtp-mail.outlook.com";
 
 
-    public static string ImgurId => "";
+    public static string ImgurId => "45017b36adc5083";
 
-    public static string ImgurSecret => "";
+    public static string ImgurSecret => "e1c3d1e4735b4de48bfa0d99aa06150d9a7c3bba";
 
-    public static string YandexId => "";
+    public static string YandexId => "3afd5060e3c74c978661d4e1c4271aa9";
 }
